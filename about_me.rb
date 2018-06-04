d63feb9a124487cc9d4f4185e7d6ac4fdaa7ec21
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-04 21:56:24 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -5,4 +5,11 @@ def about_me
   puts "I am lactose intolorant, but I still like ice cream"
 end
 
+def about_max
+  puts "Max is Giuliana's friend"
+  puts "Max is left handed"
+  puts "Max like sweet things"
+  puts "is a bae"
+
 about_me
+about_max
